66b92d7f3c6908cf91580301e78807526da0e4dc
Henrique Lima Borges - TI <hlborges@bssp.local>
2017-02-23 11:49:37 -0300

    testes :wq

--- ReACT/urls.py
+++ ReACT/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-
 from ReACT_API.api import AtletaResource
+from django.http.request import (
+    HttpRequest, QueryDict, RawPostDataException, UnreadablePostError,
+)
 
 atleta_resource = AtletaResource()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(atleta_resource.urls)),
+    '''url(HttpResponse.GET, include(atleta_resource.urls)),
+    if request.method == "GET":
+        include(atleta_resource.urls)'''
 ]
